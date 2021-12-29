7ffa5c25d194b0becee45071277b5a6f2ed21690
Anthony Vu <anthony_vu@live.ca>
2021-12-29 15:36:56 -0800

    moved gitignore to root folder

--- frontend/public/env.js
+++
@@ -1,7 +0,0 @@
-window.env = {
-  "DEV_URL": "http://localhost:8080/api/todos",
-  "PROD_URL": "https://pacific-shore-10116.herokuapp.com/api/todos",
-  "AUTH0_DOMAIN": "dev-inopepte.us.auth0.com",
-  "AUTH0_CLIENT_ID": "EoqgGRF9QTUUBbktF7ipvmQq1I78teWN",
-  "AUTH0_AUDIENCE": "https://todolist-api.example.com"
-};
